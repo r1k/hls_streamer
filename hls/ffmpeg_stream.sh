0c0b723188b41f46d6b8683abb8283b4632d51a8
r1k <3041753+r1k@users.noreply.github.com>
2023-03-22 18:39:25 +0000

    Working

--- hls/ffmpeg_stream.sh
+++ hls/ffmpeg_stream.sh
@@ -1,14 +1,39 @@
 #!/bin/sh
 # for Linux
 
-DATA_DIR="/root/hls/data"
+DATA_DIR="/root/hls/web/data"
 
-rm -rf $DATA_DIR || true
 mkdir -p $DATA_DIR || true
+rm -rf $DATA_DIR/*.ts $DATA_DIR/*.m3u8 || true
 
-ffmpeg -i input -f mpegts udp://$SRC_UDP?pkt_size=188&buffer_size=65535 \
-       -c:v libx264 -vf format=yuv420p -preset veryfast -b:v 1000k \
-       -b:a 128k \
+# Hardware accelerated - not working
+#ffmpeg -i udp://"${SRC_UDP}" \
+#       -c:v h264_vaapi -vaapi_device /dev/dri/card0 -vf 'format=nv12|vaapi,hwupload,scale_vaapi=w=1280:h=720' -b:v 1M \
+#       -c:a aac -b:a 128k \
+#       -f hls \
+#       -hls_list_size 5 \
+#       -hls_flags independent_segments \
+#       -hls_flags delete_segments \
+#       -hls_segment_type mpegts \
+#       -hls_segment_filename $DATA_DIR/segment_%05d.ts \
+#       -hls_base_url $ROOT_URL/data/  $DATA_DIR/playlist.m3u8
+
+# Transcode variant
+#ffmpeg -i udp://"${SRC_UDP}" \
+#       -c:v libx264 -x264-params "nal-hrd=cbr" -vf format=yuv420p,scale=-1:720 -preset veryfast -b:v 1M -minrate 1M -maxrate 1M -bufsize 2M \
+#       -c:a aac -b:a 128k \
+#       -f hls \
+#       -hls_list_size 5 \
+#       -hls_flags independent_segments \
+#       -hls_flags delete_segments \
+#       -hls_segment_type mpegts \
+#       -hls_segment_filename $DATA_DIR/segment_%05d.ts \
+#       -hls_base_url $ROOT_URL/data/  $DATA_DIR/playlist.m3u8
+
+# Copy original encoding
+ffmpeg -i udp://"${SRC_UDP}" \
+       -c:v copy \
+       -c:a aac -b:a 128k \
        -f hls \
        -hls_list_size 5 \
        -hls_flags independent_segments \
